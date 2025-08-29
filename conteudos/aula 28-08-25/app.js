fb508d647cb8d8b7956b4186cef158ff5379d1fc
ILMoretto <morete.alencar@gmail.com>
2025-08-28 22:08:39 -0400

    exercicio calculadora

---
+++ conteudos/aula 28-08-25/app.js
@@ -0,0 +1,8 @@
+function somar() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+
+    let soma = Number(num1) + Number(num2);
+
+    alert("O resultado da soma é: " + soma);
+}
